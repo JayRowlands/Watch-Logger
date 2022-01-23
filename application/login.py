eba6e0ed58a8deb9da1a62e2e645509076fc2325
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2022-01-23 01:13:20 +0000

    Users are placed in a listbox to select which one to login to

--- application/login.py
+++ application/login.py
@@ -2,27 +2,49 @@ import PySimpleGUI as sg
 import sqlite3
 
 def main():
+    user_list = getUsers()
     sg.theme('DarkGrey6')
 
-    layout = [[sg.Text('Enter Username')],      
-            [sg.Input(key='Username', do_not_clear=False)],      
+    layout = [[sg.Text('Enter Username')],
+            [sg.Listbox(key='User_List', values=user_list, size=(40, 10), enable_events=True,)],   
+            [sg.Input(key='Username', do_not_clear=False, enable_events=True)],      
             [sg.Button('Submit'), sg.Exit()]]      
 
     login = sg.Window('Watch Logger', layout)      
-    event, values = login.read() 
-    createUser(values.get('Username'))
+ 
+    while True:
+        event, values = login.Read()
+        if event is None or event == 'Exit':                # always check for closed window
+            break
+        if event == 'User_List' and len(values['User_List']):     # if a list item is chosen
+            print(values.get("User_List"))
+            login.find_element('Username').Update(values.get("User_List"))
+    login.Close()
+    #createUser(values.get('Username'))
+
+
+def login(username):
+    print("to do")
 
 def createUser(username):
     db = sqlite3.connect('logger.sqlite3')
     cursor = db.cursor()
     cursor.execute(f"INSERT INTO Users (name) VALUES ('{username}')")
-
-    cursor.execute("SELECT * FROM Users")
-    users = cursor.fetchall()
-    for i in users:
-        print(i)
     db.commit()
     db.close()
+
+def getUsers():
+    db = sqlite3.connect('logger.sqlite3')
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM Users")
+    users = [item[0] for item in cursor.fetchall()]
+    
+    user_list = []
+    for i in users:
+        user_list.append(i)
+    db.close()
+     
+    return user_list
 
 if __name__ == "__main__":
     main()
