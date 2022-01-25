07aeee63db47db40fb4a0f1046899b8251b234bd
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2022-01-25 14:26:26 +0000

    Can delete a user and the db/ UI is updated

--- application/login.py
+++ application/login.py
@@ -8,17 +8,23 @@ def main():
     layout = [[sg.Text('Enter Username')],
             [sg.Listbox(key='User_List', values=user_list, size=(40, 10), enable_events=True,)],   
             [sg.Input(key='Username', do_not_clear=False, enable_events=True)],      
-            [sg.Button('Submit'), sg.Exit()]]      
+            [sg.Button('Login'), sg.Button('Create User'), sg.Button('Delete User'), sg.Exit()]]      
 
-    login = sg.Window('Watch Logger', layout)      
+    login = sg.Window('Watch Logger', layout).Finalize()      
  
     while True:
         event, values = login.Read()
         if event is None or event == 'Exit':                # always check for closed window
             break
         if event == 'User_List' and len(values['User_List']):     # if a list item is chosen
-            print(values.get("User_List"))
             login.find_element('Username').Update(values.get("User_List"))
+        if event == 'Create User':
+            createUser(values.get('Username'))
+        if event == 'Delete User':
+            if (values.get('Username') != ""):
+                deleteUser(values.get('Username'))
+                login.find_element('User_List').Update(values=getUsers())
+                event, values = login.Read()
     login.Close()
     #createUser(values.get('Username'))
 
@@ -33,17 +39,26 @@ def createUser(username):
     db.commit()
     db.close()
 
+def deleteUser(username):
+    db = sqlite3.connect('logger.sqlite3')
+    cursor = db.cursor()
+    list_of_chars = ['(', ',', ')']
+    for c in list_of_chars:
+        username = username.replace(c, '')
+    username= username.strip(" ' ")
+    cursor.execute(f"DELETE FROM Users WHERE name = '{username}'")
+    db.commit()
+    db.close()
+
 def getUsers():
     db = sqlite3.connect('logger.sqlite3')
     cursor = db.cursor()
     cursor.execute("SELECT name FROM Users")
     users = [item[0] for item in cursor.fetchall()]
-    
     user_list = []
     for i in users:
         user_list.append(i)
     db.close()
-     
     return user_list
 
 if __name__ == "__main__":
